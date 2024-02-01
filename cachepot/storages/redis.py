9b31ecea2eaafca31d27defc7e3fbb9a377e9e64
Alexey Kotenko <alexey@kotenko.me>
2024-02-01 17:40:58 +0000

    Fix redis storage

--- cachepot/storages/redis.py
+++ cachepot/storages/redis.py
@@ -6,7 +6,7 @@ from cachepot.storages.abstract import AbstractStorage
 
 
 class RedisStorage(AbstractStorage):
-    def __init__(self, redis: Redis[bytes]):
+    def __init__(self, redis: 'Redis[bytes]'):
         assert isinstance(redis, Redis), 'Invalid Redis client passed'
         self.redis: Redis[bytes] = redis
 
@@ -14,7 +14,7 @@ class RedisStorage(AbstractStorage):
         data = await self.redis.get(key)
         if data and not isinstance(data, bytes):
             return bytes(data)
-        return None
+        return data
 
     async def set(self, key: str, value: bytes, expire: Optional[int] = None) -> bool:
         await self.redis.set(key, value, ex=expire)
